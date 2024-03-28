383752026fac4a72a98fa85d751295ef303cfca0
pablo segura barranco <89455065+palolo25@users.noreply.github.com>
2024-03-29 00:26:23 +0100

    Primer commit

---
+++ mystock/src/test/java/app/web/control/stock/mystock/MystockApplicationTests.java
@@ -0,0 +1,13 @@
+package app.web.control.stock.mystock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MystockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
